dee7066046da05b610cc7567d084c16176874606
Wazen Shbair <engwazen@gmail.com>
2017-06-12 21:11:15 +0200

    parsing certificate in golang

---
+++ Parsing-Certificate-GoLang/pars.go
@@ -0,0 +1,35 @@
+// This code parses the x.509 certificates that include extension for blockchain
+// Author Wazen SHBAIR
+// Snt, university of Luxembourg
+
+package main
+import (
+	"crypto/x509"
+	"io/ioutil"
+	"fmt"
+)
+
+func main() {
+	// Certificate types
+	//BlockchainTestRootCA3 % BlockchainEndUser % BlockchainTestIssuingCA3
+	cert:="BlockchainTestIssuingCA3.cer"
+	fmt.Println("\tParsing : " + cert)
+	fmt.Println("\t--------------------------------")
+	certPEMBlock, _ := ioutil.ReadFile(cert)
+	ca, _ := x509.ParseCertificate(certPEMBlock)
+	// iterate in the extension to get the information
+	for _, element := range ca.Extensions {
+		if element.Id.String() == "1.2.752.115.33.3"{ //<-- this string comes from the certifcate it self
+			fmt.Printf("\tBlockChain Name: %+#+x\n", element.Value)
+		}
+		if element.Id.String() == "1.2.752.115.33.2"{
+			fmt.Printf("\tCaContractIdentifier: %+#+x\n", element.Value)
+		}
+		if element.Id.String() == "1.2.752.115.33.1"{
+			fmt.Printf("\tIssuerCaContractIdentifier: %+#+x\n", element.Value)
+		}
+		if element.Id.String() == "1.2.752.115.33.0"{
+			fmt.Printf("\tHash Algo: %+#+x\n", element.Value)
+		}
+	}
+}
